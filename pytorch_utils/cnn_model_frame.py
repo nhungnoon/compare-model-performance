be294a7d282d78915c9d97d1500d5eb7e7c1ecb5
nhungnoon <43654499+nhungnoon@users.noreply.github.com>
2024-03-26 21:49:11 -0400

    Create cnn_model_frame.py

---
+++ pytorch_utils/cnn_model_frame.py
@@ -0,0 +1,40 @@
+"""
+A customized CNN model
+"""
+
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch.utils.data import Dataset, TensorDataset
+from torchvision import transforms
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import os
+import cv2
+from torchvision.transforms import v2
+
+class CNN_model(nn.Module):
+    def __init__(self):
+        super(CNN_model, self).__init__()
+        self.conv1 = nn.Conv2d(1, 16, (3, 3))
+        self.convnorm1 = nn.BatchNorm2d(16)
+        self.drop = nn.Dropout(0.25)
+        self.pool1 = nn.MaxPool2d((2, 2))
+
+        self.conv2 = nn.Conv2d(16, 32, (3, 3))
+        self.convnorm2 = nn.BatchNorm2d(32)
+        self.pool2 = nn.AvgPool2d((2, 2))
+
+        self.linear1 = nn.Linear(32 * 5 * 5, 400)
+        self.linear1_bn = nn.BatchNorm1d(400)
+        self.drop = nn.Dropout(0.25)
+        self.linear2 = nn.Linear(400, 10)
+        self.act = torch.relu
+
+    def forward(self, x):
+        x = self.pool1(self.convnorm1(self.act(self.conv1(x))))
+        x = self.pool2(self.convnorm2(self.act(self.conv2(x))))
+        x = self.drop(self.linear1_bn(self.act(self.linear1(x.view(len(x), -1)))))
+        return self.linear2(x)
