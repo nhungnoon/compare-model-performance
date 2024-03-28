f0689feedf05b0d0541d85bdc8880a496f7de242
nhungnoon <43654499+nhungnoon@users.noreply.github.com>
2024-03-27 22:04:19 -0400

    update shape dimension

--- pytorch_utils/cnn_model_frame.py
+++ pytorch_utils/cnn_model_frame.py
@@ -7,21 +7,21 @@ import torch.nn as nn
 
 # based on 
 # https://github.com/nhungnoon/Final-Project-Group3/blob/master/Nhung-Nguyen-individual-project/Code/pytorch_ver.py
-class CNN_model(nn.Module):
+class CNN(nn.Module):
     def __init__(self):
-        super(CNN_model, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, (3, 3))
-        self.convnorm1 = nn.BatchNorm2d(16)
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, (3, 3),)
+        self.convnorm1 = nn.BatchNorm2d(32)
         self.drop = nn.Dropout(0.25)
         self.pool1 = nn.MaxPool2d((2, 2))
 
-        self.conv2 = nn.Conv2d(16, 32, (3, 3))
-        self.convnorm2 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, (3, 3))
+        self.convnorm2 = nn.BatchNorm2d(64)
         self.pool2 = nn.AvgPool2d((2, 2))
 
-        self.linear1 = nn.Linear(32 * 5 * 5, 400)
+        self.linear1 = nn.Linear(64 * 5 * 5, 400)
         self.linear1_bn = nn.BatchNorm1d(400)
-        self.drop = nn.Dropout(0.25)
+        self.drop = nn.Dropout(0.5)
         self.linear2 = nn.Linear(400, 10)
         self.act = torch.relu
 
