755797e3a7f8faf5d8f9bb9a210f6fe7a8d66079
nhungnoon <nhung@local>
2024-03-29 21:53:53 -0400

    add sequential model frame

---
+++ tf_sequential_utils/sequential_model_frame.py
@@ -0,0 +1,30 @@
+"""
+A sequential model  
+"""
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Conv2D, Dense, Flatten, Dropout
+
+
+class TFSequentialModel(Model):
+
+    def __init__(self, **kwargs):
+        super(TFSequentialModel, self).__init__(**kwargs)
+
+        self.conv_1 = Conv2D(32, (3, 3), strides=2, activation="relu")
+        self.drop_layer = Dropout(0.5)
+        self.conv_2 = Conv2D(32, (3, 3))
+        self.flatten = Flatten()
+        self.dense = Dense(128, activation="relu")
+        self.final_dense = Dense(10, activation="softmax")
+
+    def call(self, inputs):
+
+        x = self.conv_1(inputs)
+        x = self.drop_layer(x)
+        x = self.conv_2(x)
+        x = self.flatten(x)
+        x = self.dense(x)
+        x = self.final_dense(x)
+
+        return x
