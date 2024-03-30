a7e78c14039a0b9115359bae3424c9572bf0c2f8
nhungnoon <43654499+nhungnoon@users.noreply.github.com>
2024-03-29 22:07:49 -0400

    add loss info

--- tf_sequential_utils/sequential_model_frame.py
+++ tf_sequential_utils/sequential_model_frame.py
@@ -6,25 +6,26 @@ Based on https://www.tensorflow.org/guide/keras/making_new_layers_and_models_via
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Conv2D, Dense, Flatten, Dropout
 
-class TFSequentialModel(Model):
+class SequentialModel(Model, Layer):
 
-    def __init__(self, **kwargs):
-        super(TFSequentialModel, self).__init__(**kwargs)
-
-        self.conv_1 = Conv2D(32, (3, 3), strides=2, activation="relu")
+    def __init__(self, rate=1e-2, **kwargs):
+        super(SequentialModel, self).__init__(**kwargs)
+        self.rate = rate
+        self.conv_1 = Conv2D(32, (3,3), strides=2, activation="relu")
         self.drop_layer = Dropout(0.5)
-        self.conv_2 = Conv2D(32, (3, 3))
+        self.conv_2 = Conv2D(32, (3,3))
         self.flatten = Flatten()
-        self.dense = Dense(128, activation="relu")
-        self.final_dense = Dense(10, activation="softmax")
-
+        self.dense = Dense(128, activation='relu')
+        self.final_dense = Dense(10, activation='softmax')
+        
     def call(self, inputs):
 
         x = self.conv_1(inputs)
+        self.add_loss(self.rate * tf.reduce_mean(inputs))
         x = self.drop_layer(x)
         x = self.conv_2(x)
         x = self.flatten(x)
         x = self.dense(x)
         x = self.final_dense(x)
-
+        
         return x
