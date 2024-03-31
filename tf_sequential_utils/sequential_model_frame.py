4d4c94175627cb833fa72dffaaeb4eb96ebbd1a7
nhungnoon <43654499+nhungnoon@users.noreply.github.com>
2024-03-31 12:24:17 -0400

    Format

--- tf_sequential_utils/sequential_model_frame.py
+++ tf_sequential_utils/sequential_model_frame.py
@@ -6,17 +6,18 @@ Based on https://www.tensorflow.org/guide/keras/making_new_layers_and_models_via
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Conv2D, Dense, Flatten, Dropout
 
+
 class SequentialModel(Model, Layer):
 
     def __init__(self, rate=1e-2, **kwargs):
         super(SequentialModel, self).__init__(**kwargs)
         self.rate = rate
-        self.conv_1 = Conv2D(32, (3,3), strides=2, activation="relu")
+        self.conv_1 = Conv2D(32, (3, 3), strides=2, activation="relu")
         self.drop_layer = Dropout(0.5)
-        self.conv_2 = Conv2D(32, (3,3))
+        self.conv_2 = Conv2D(32, (3, 3))
         self.flatten = Flatten()
-        self.dense = Dense(128, activation='relu')
-        self.final_dense = Dense(10, activation='softmax')
+        self.dense = Dense(128, activation="relu")
+        self.final_dense = Dense(10, activation="softmax")
 
     def build(self, input_shape):
         self.w = self.add_weight(
@@ -37,7 +38,7 @@ class SequentialModel(Model, Layer):
         x = self.flatten(x)
         x = self.dense(x)
         x = self.final_dense(x)
-        
+
         return x
 
 
@@ -48,28 +49,28 @@ def find_loss_gradients(model, inputs, targets, loss):
         # Add extra losses
         loss_update += sum(model.losses)
     grad_value = t.gradient(loss_update, model.trainable_variables)
-        
+
     return loss_update, grad_value
 
 
 def train_model(model, num_epochs, dataset, optimizer, loss, grad_fn):
-    
+
     train_loss_results = []
     train_accuracy_results = []
     for epoch in range(num_epochs):
 
-      epoch_loss_avg = tf.keras.metrics.Mean()
-      epoch_accuracy = tf.keras.metrics.CategoricalAccuracy()
+        epoch_loss_avg = tf.keras.metrics.Mean()
+        epoch_accuracy = tf.keras.metrics.CategoricalAccuracy()
 
-      for x, y in train_dataset:
+        for x, y in train_dataset:
 
-        loss_value, grads = grad_fn(model, x, y, loss)
-        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+            loss_value, grads = grad_fn(model, x, y, loss)
+            optimizer.apply_gradients(zip(grads, model.trainable_variables))
 
-        epoch_loss_avg(loss_value)
-        epoch_accuracy(to_categorical(y), model(x))
+            epoch_loss_avg(loss_value)
+            epoch_accuracy(to_categorical(y), model(x))
 
-      train_loss_results.append(epoch_loss_avg.result())
-      train_accuracy_results.append(epoch_accuracy.result())
+        train_loss_results.append(epoch_loss_avg.result())
+        train_accuracy_results.append(epoch_accuracy.result())
 
     return train_loss_results, train_accuracy_results
