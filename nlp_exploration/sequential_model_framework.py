536d4ec77b4266d1df1500d0f0d037e501bd46a2
nhungnoon <nhung@local>
2024-04-12 21:22:24 -0400

    update nlp framework

---
+++ nlp_exploration/sequential_model_framework.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from keras import regularizers
+from tensorflow.keras.layers import Layer, Dense, Dropout
+
+
+class NLPSequentialModel(Model, Layer):
+
+    def __init__(self, **kwargs):
+        super(NLPSequentialModel, self).__init__(**kwargs)
+        self.dense_1 = Dense(
+            128,
+            activation="relu",
+            kernel_regularizer=regularizers.l1(0.001),
+        )
+        self.drop_layer = Dropout(0.5)
+        self.dense_2 = Dense(
+            64,
+            activation="relu",
+            kernel_regularizer=regularizers.l2(0.001),
+        )
+        self.final_dense = Dense(46, activation="softmax")
+
+    def build(self, input_shape):
+        self.w = self.add_weight(
+            shape=(input_shape[-1], 64),
+            initializer="random_uniform",
+            trainable=True,
+        )
+        self.b = self.add_weight(
+            shape=(64,), initializer="random_normal", trainable=True
+        )
+
+    def call(self, inputs):
+        inputs = tf.matmul(inputs, self.w) + self.b
+        x = self.dense_1(inputs)
+        x = self.drop_layer(x)
+        x = self.dense_2(x)
+        x = self.final_dense(x)
+
+        return x
