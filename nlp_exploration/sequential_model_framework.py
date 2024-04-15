e31b206b67542ebe5336c812fc8699b77dbf72fb
nhungnoon <nhung@local>
2024-04-14 20:30:08 -0400

    update framework and add in helper function

--- nlp_exploration/sequential_model_framework.py
+++ nlp_exploration/sequential_model_framework.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Model
 from keras import regularizers
@@ -19,6 +20,7 @@ class NLPSequentialModel(Model, Layer):
             64,
             activation="relu",
             kernel_regularizer=regularizers.l2(0.001),
+            kernel_initializer="glorot_uniform",
         )
         self.drop_layer2 = Dropout(0.25)
         # TODO make the class number flexible
@@ -43,3 +45,10 @@ class NLPSequentialModel(Model, Layer):
         x = self.final_dense(x)
 
         return x
+
+
+def transform_data(x_array, buffer_size=5000):
+    results = np.zeros((len(x_array), buffer_size))
+    for i, j in enumerate(x_array):
+        results[i, j] = 1.0
+    return results
