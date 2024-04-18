321b2f33ef4682c7fcc9a04da6c217ee2e767898
nhungnoon <43654499+nhungnoon@users.noreply.github.com>
2024-04-17 21:50:55 -0400

    add additional model to experiment

--- nlp_exploration/lr_model_framework.py
+++ nlp_exploration/lr_model_framework.py
@@ -1,4 +1,18 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.tree import DecisionTreeClassifier
+
 
 def lr_model_for_nlp():
   return LogisticRegression(C= 0.8, class_weight="balanced")
+
+def dt_classifier():
+  dt_classifier = DecisionTreeClassifier(
+    random_state=0,
+    min_samples_split=5,
+    class_weight="balanced",
+  )
+  return dt_classifier
+
+def multinominal_nb():
+  return MultinomialNB(alpha=0.8)
