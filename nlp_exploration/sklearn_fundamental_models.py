9bbe252e4cec533a21dbebc3450929c8aa124815
nhungnoon <43654499+nhungnoon@users.noreply.github.com>
2024-04-20 21:39:03 -0400

    add ridge classifier

--- nlp_exploration/sklearn_fundamental_models.py
+++ nlp_exploration/sklearn_fundamental_models.py
@@ -1,5 +1,5 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, RidgeClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.tree import DecisionTreeClassifier
 
@@ -26,3 +26,7 @@ def rf_classifier():
         max_depth=3, min_samples_split=5, class_weight="balanced", random_state=16
     )
     return rf_classified
+
+def ridgeclassifier():
+    ridge_classifier = RidgeClassifier(class_weight="balanced", random_state=0)
+    return ridge_classifier
